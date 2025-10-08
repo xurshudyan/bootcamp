1eb8dad0490365a7001b585f246b58f948b20684
Xurshudyan <v.khurshudyan@aico.swiss>
2025-10-08 18:30:24 +0400

    Force https

--- config/services.php
+++ config/services.php
@@ -38,6 +38,6 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'https://bootcamp.on-forge.com/auth/callback',
+        'redirect' => env('GITHUB_REDIRECT_URL', 'https://bootcamp.on-forge.com/auth/callback'),
     ],
 ];
